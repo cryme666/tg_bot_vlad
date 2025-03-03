130e93b30f003a4cc2930e86b4fb4391e3dd0ee7
cryme666 <125838508+cryme666@users.noreply.github.com>
2025-03-03 20:00:43 +0200

    upd

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import os
 from dotenv import load_dotenv
+import telebot
 
 load_dotenv()
 
 BOT_KEY = os.getenv("BOT_KEY")
+ADMIN = os.getenv("admin")
+admins = [int(admin) for admin in ADMIN.split(',')]
 
-print(BOT_KEY)
+bot = telebot.TeleBot(BOT_KEY)
+
+@bot.message_handler(commands=['start'])
+def hello(message):
+    bot.send_message(message.chat.id,'Hello!')
+
+def start(admin):
+    for admin in admins:
+        bot.send_message(admin,'Bot started!')
+
+
+if __name__ =='__main__':
+    bot.polling(non_stop=True)
+    start(admins)
